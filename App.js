69b367a2008471a09742f83ada2a3785f98cfd36
fadishurrush@gmail.com <114020115+fadishurrush@users.noreply.github.com>
2023-06-14 13:58:24 +0300

    new database

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const App = express();
 const mongoose = require("mongoose");
 const UserRoute = require("./api/routes/UserRoute")
 mongoose.connect(
-  "mongodb+srv://Fadishurrush:krkIQCsSxTLXxA9Z@cluster0.ybiycvx.mongodb.net/?retryWrites=true&w=majority"
+  "mongodb+srv://malek:123456780@test.c38nkyc.mongodb.net/?retryWrites=true&w=majority"
 );
 
 mongoose.connection.on("connected", () => {
